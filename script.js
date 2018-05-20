8d223ac5a58d5834652faa2dad278387aa288a1e
Huongg <32060364+Huongg@users.noreply.github.com>
2018-05-20 21:01:46 +0100

    Added calculator and updateOperator functions

--- script.js
+++ script.js
@@ -1,24 +1,64 @@
+
 $(document).ready(function(){
-		let number= "";
-		let newNumber = "";
+		let leftNumber= "";
+		let rightNumber = "";
+		let hadOperator = false;
 		let operator ="";
+		let result = ""; 
 		let totaldiv = $("#total");
     	totaldiv.text("0");
 
 
 		$("#numbers a").not("#all, #clearall").click(function(){
-			number += $(this).text();
-			totaldiv.text(number);
+			if (hadOperator ==false){
+				leftNumber += $(this).data('value');
+				totaldiv.text(leftNumber);
+				console.log(`leftNumber is ${leftNumber}`);
+			} else {
+				rightNumber += $(this).data('value');
+				totaldiv.text(rightNumber);
+				console.log(`rightNumber is ${rightNumber}`);
+				calculate ();
+			}
 		})
 
 		$("#operators a").not("#equals").click(function(){
-			operator = $(this).text();
-			$(this).css("color", "red");
-			newNumber = number;
-			number = "";
+			hadOperator = true;
+			operator = $(this).data('value');
+			updateOperatorsColour ($(this));
 
+			if (leftNumber !== "" && rightNumber !== ""){
+				totaldiv.text(result);
+				leftNumber = result;
+				rightNumber ="";
+				console.log(`result is ${leftNumber}`);
+			} 
 		})
 
+		$("#clear,#clearall").click(function(){
+			leftnumber = "";
+			rightNumber="";
+			result="";
+			totaldiv.text("0");		
+	    });
+
+	    $("#equals").click(function(){
+	    	updateOperatorsColour ($(this));
+	    	calculate();
+	    	totaldiv.text(result);
+	    });
+
+	    function calculate (){
+	    	result = eval(leftNumber + operator + rightNumber);
+	    }
+
+	    function updateOperatorsColour (item){
+	    	if ($("#operators a").hasClass("selected-operator")){
+	    		$("#operators a").removeClass("selected-operator");
+	    	};
+	    	item.addClass("selected-operator");
+	    	
+	    }
 
 })
 
